65af70328069df2b24c172c2b8eb793aff618eb5
Vijayarajamayuri <avrmayuri74@gmail.com>
2020-02-19 16:56:24 -0600

    maximum function mapper and reducer

---
+++ maximum-of-residential-units/reducer.py
@@ -0,0 +1,32 @@
+s = open("02.txt","r")
+r = open("03.txt", "w")
+
+thisKey = ""
+thisValue = 0.0
+
+for line in s:
+  data = line.strip().split('\t')
+  yearbuilt, residentialunits = data
+
+  if yearbuilt != thisKey:
+    if thisKey:
+      # output the last key value pair result
+      r.write(thisKey + '\t' + str(thisValue)+'\n')
+
+    # start over when changing keys
+    thisKey = yearbuilt 
+    thisValue = 0.0
+  
+  # apply the aggregation function - now maximum
+  #thisValue += float(amount)
+  if (int(residentialunits) > int(thisValue)):
+    thisValue =int(residentialunits) 
+
+# output the final entry when done
+r.write(thisKey + '\t' + str(thisValue)+'\n')
+
+
+
+s.close()
+r.close()
+
