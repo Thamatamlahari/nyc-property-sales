fe36dc9ad6d3c31d067b8cea0525039e8e273dad
Vijayarajamayuri <avrmayuri74@gmail.com>
2020-02-19 17:22:12 -0600

    count of the residential units

--- count-residential-units/mapper.py
+++ count-residential-units/mapper.py
@@ -1,1 +1,10 @@
-hi
+input = open("data.txt", "r")
+output = open("01.txt", "w")
+
+for line in input:
+    datalist = line.strip().split("\t")
+    sno, boroug, neighborhood, buildingcategory, taxpresent, block, lot, easement, buildingatpresent, address, apartmentnumber, zipcode, residentialunits, commercialunits, totalunits, landsquarefeet, grosssquarefeet, yearbuilt, taxatsale, buildingatsale, saleprice, saledate = datalist
+    output.write(yearbuilt + "\t" + residentialunits + "\n")
+
+input.close()
+output.close()
---
+++ count-residential-units/reducer.py
@@ -0,0 +1,31 @@
+s = open("02.txt","r")
+r = open("03.txt", "w")
+
+thisKey = ""
+thisValue = 0.0
+
+for line in s:
+  data = line.strip().split('\t')
+  yearbuilt, residentialunits = data
+
+  if yearbuilt != thisKey:
+    if thisKey:
+      # output the last key value pair result
+      r.write(thisKey + '\t' + str(thisValue)+'\n')
+
+    # start over when changing keys
+    thisKey = yearbuilt 
+    thisValue = 0.0
+  
+  # apply the aggregation function - now maximum
+  #thisValue += float(amount)
+  thisValue += 1
+
+# output the final entry when done
+r.write(thisKey + '\t' + str(thisValue)+'\n')
+
+
+
+s.close()
+r.close()
+
