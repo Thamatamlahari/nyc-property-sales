6b1645986fd150772f9cd9db869013843ed754b5
Vijayarajamayuri <avrmayuri74@gmail.com>
2020-02-21 14:17:29 -0600

    commented mappers and reducers and screenshots

--- maximum-of-residential-units/mapper.py
+++ maximum-of-residential-units/mapper.py
@@ -1,9 +1,14 @@
 input = open("data.txt", "r")
+#reading the input from data.txt file
 output = open("01.txt", "w")
+#reading the output to 01.txt file
 
 for line in input:
+    #the data is seperated by the tab space (/t)
     datalist = line.strip().split("\t")
+    #The fields are given to the datalist field
     sno, boroug, neighborhood, buildingcategory, taxpresent, block, lot, easement, buildingatpresent, address, apartmentnumber, zipcode, residentialunits, commercialunits, totalunits, landsquarefeet, grosssquarefeet, yearbuilt, taxatsale, buildingatsale, saleprice, saledate = datalist
+    #Two field are taken yearbuilt and residental units for the max function 
     output.write(yearbuilt + "\t" + residentialunits + "\n")
 
 input.close()
--- maximum-of-residential-units/reducer.py
+++ maximum-of-residential-units/reducer.py
@@ -1,13 +1,13 @@
 s = open("02.txt","r")
 r = open("03.txt", "w")
-
+# reading the input from 02.txt and writing into the 03.txt
 thisKey = ""
 thisValue = 0.0
 
 for line in s:
   data = line.strip().split('\t')
   yearbuilt, residentialunits = data
-
+ 
   if yearbuilt != thisKey:
     if thisKey:
       # output the last key value pair result
@@ -18,10 +18,10 @@ for line in s:
     thisValue = 0.0
   
   # apply the aggregation function - now maximum
-  #thisValue += float(amount)
+
   if (int(residentialunits) > int(thisValue)):
     thisValue =int(residentialunits) 
-
+#finding the max of residential units
 # output the final entry when done
 r.write(thisKey + '\t' + str(thisValue)+'\n')
 
